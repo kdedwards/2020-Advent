5a843647c904a23c463ab902d84b6390c6ebe87f
Dwayne Edwards <kdedwards@gmail.com>
2020-12-07 08:09:24 -0500

    Day 1

---
+++ Day 01/Report_Repair.py
@@ -0,0 +1,15 @@
+from pprint import pprint
+
+with open("C:\\Users\\kdedw\\projects\\2020-Advent\\Day 01\\Report_Repair.dat") as expenseData:
+    expenses = expenseData.read().split('\n')
+#pprint(expenses)
+for expense in expenses:
+    for secExpense in expenses[expenses.index(expense)+1::]:
+        if(int(expense)+int(secExpense)==2020):
+            print('Found it! Expense1: {} Expense2: {} Product {}'.format(expense, secExpense, int(expense)*int(secExpense)))
+
+for expense in expenses:
+    for secExpense in expenses[expenses.index(expense)+1::]:
+        for thirExpense in expenses[expenses.index(secExpense)+1::]:
+            if(int(expense)+int(secExpense)+int(thirExpense)==2020):
+                print('Found it! Expense1: {} Expense2: {} Expense3: {} Product {}'.format(expense, secExpense, thirExpense, int(expense)*int(secExpense)*int(thirExpense)))
