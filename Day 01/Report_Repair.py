49a7d334042890c36570686cea0034ae24e42b84
Dwayne Edwards <kdedwards@gmail.com>
2020-12-23 01:23:32 -0500

    Fixing linting errors

--- Day 01/Report_Repair.py
+++ Day 01/Report_Repair.py
@@ -2,12 +2,12 @@ with open("Report_Repair.dat") as expenseData:
     expenses = expenseData.read().split('\n')
 
 for expense in expenses:
-    for secExpense in expenses[expenses.index(expense)+1::]:
-        if(int(expense)+int(secExpense)==2020):
-            print('Found it! Expense1: {} Expense2: {} Product {}'.format(expense, secExpense, int(expense)*int(secExpense)))
+    for secExpense in expenses[expenses.index(expense) + 1::]:
+        if(int(expense) + int(secExpense) == 2020):
+            print('Found it! Expense1: {} Expense2: {} Product {}'.format(expense, secExpense, int(expense) * int(secExpense)))
 
 for expense in expenses:
-    for secExpense in expenses[expenses.index(expense)+1::]:
-        for thirExpense in expenses[expenses.index(secExpense)+1::]:
-            if(int(expense)+int(secExpense)+int(thirExpense)==2020):
-                print('Found it! Expense1: {} Expense2: {} Expense3: {} Product {}'.format(expense, secExpense, thirExpense, int(expense)*int(secExpense)*int(thirExpense)))
+    for secExpense in expenses[expenses.index(expense) + 1::]:
+        for thirExpense in expenses[expenses.index(secExpense) + 1::]:
+            if(int(expense) + int(secExpense) + int(thirExpense) == 2020):
+                print('Found it! Expense1: {} Expense2: {} Expense3: {} Product {}'.format(expense, secExpense, thirExpense, int(expense) * int(secExpense) * int(thirExpense)))
--- Day 03/trajectory.py
+++ Day 03/trajectory.py
@@ -1,60 +1,60 @@
 with open("map.dat") as mapData:
     tobogganMap = mapData.read().split('\n')
 
-column=1
-treeCount=0
+column = 1
+treeCount = 0
 for row in tobogganMap:
-    if(row[column % len(row)-1] == '#') :
-        treeCount+=1
-    column+=3
+    if(row[column % len(row) - 1] == '#'):
+        treeCount += 1
+    column += 3
 print('Trees hit: {}'.format(treeCount))
 
 # R1, D1
-column=1
-treeCountR1D1=0
+column = 1
+treeCountR1D1 = 0
 for row in tobogganMap:
-    if(row[column % len(row)-1] == '#') :
-        treeCountR1D1+=1
-    column+=1
+    if(row[column % len(row) - 1] == '#'):
+        treeCountR1D1 += 1
+    column += 1
 print('Trees hit (R1D1): {}'.format(treeCountR1D1))
 
 # R3, D1
-column=1
-treeCountR3D1=0
+column = 1
+treeCountR3D1 = 0
 for row in tobogganMap:
-    if(row[column % len(row)-1] == '#') :
-        treeCountR3D1+=1
-    column+=3
+    if(row[column % len(row) - 1] == '#'):
+        treeCountR3D1 += 1
+    column += 3
 print('Trees hit (R3D1): {}'.format(treeCountR3D1))
 
 # R5, D1
-column=1
-treeCountR5D1=0
+column = 1
+treeCountR5D1 = 0
 for row in tobogganMap:
-    if(row[column % len(row)-1] == '#') :
-        treeCountR5D1+=1
-    column+=5
+    if(row[column % len(row) - 1] == '#'):
+        treeCountR5D1 += 1
+    column += 5
 print('Trees hit (R5D1): {}'.format(treeCountR5D1))
 
 # R7, D1
-column=1
-treeCountR7D1=0
+column = 1
+treeCountR7D1 = 0
 for row in tobogganMap:
-    if(row[column % len(row)-1] == '#') :
-        treeCountR7D1+=1
-    column+=7
+    if(row[column % len(row) - 1] == '#'):
+        treeCountR7D1 += 1
+    column += 7
 print('Trees hit (R7D1): {}'.format(treeCountR7D1))
 
 # R1, D2
-column=1
-treeCountR1D2=0
-idx=0
+column = 1
+treeCountR1D2 = 0
+idx = 0
 for row in tobogganMap:
-    if(idx % 2 == 0) :
-        if(row[column % len(row)-1] == '#') :
-            treeCountR1D2+=1
-        column+=1
-    idx+=1
+    if(idx % 2 == 0):
+        if(row[column % len(row) - 1] == '#'):
+            treeCountR1D2 += 1
+        column += 1
+    idx += 1
 print('Trees hit (R1D2): {}'.format(treeCountR1D2))
 
-print('Answer: {}'.format(treeCountR1D1*treeCountR3D1*treeCountR5D1*treeCountR7D1*treeCountR1D2))
+print('Answer: {}'.format(treeCountR1D1 * treeCountR3D1 * treeCountR5D1 * treeCountR7D1 * treeCountR1D2))
