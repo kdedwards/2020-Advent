46c6b67dba6c7816809634796e9dde31f8cea29d
Dwayne Edwards <kdedwards@gmail.com>
2020-12-26 00:22:55 -0500

    Fixed linting errors

--- Day 05/seatfinder-p1.py
+++ Day 05/seatfinder-p1.py
@@ -1,18 +1,18 @@
 with open("seats.dat") as seatData:
     seats = seatData.read().split('\n')
 
-highestSeatID=0
+highestSeatID = 0
 
 for seat in seats:
-    rowStr=seat[0:7]
-    colStr=seat[7:10]
-    rowBin=rowStr.replace('B','1').replace('F','0')
-    colBin=colStr.replace('R','1').replace('L','0')
-    rowDec=int(rowBin, 2)
-    colDec=int(colBin, 2)
-    seatId=rowDec*8+colDec
+    rowStr = seat[0:7]
+    colStr = seat[7:10]
+    rowBin = rowStr.replace('B', '1').replace('F', '0')
+    colBin = colStr.replace('R', '1').replace('L', '0')
+    rowDec = int(rowBin, 2)
+    colDec = int(colBin, 2)
+    seatId = rowDec * 8 + colDec
     print('Seat: {} row: {} col: {} rowBin: {} colBin: {} rowDec: {} colDec: {} seatId: {}'.format(seat, rowStr, colStr, rowBin, colBin, rowDec, colDec, seatId))
     if(seatId > highestSeatID):
-        highestSeatID=seatId
+        highestSeatID = seatId
 
 print('Highest SeatId: {}'.format(highestSeatID))
