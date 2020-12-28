0910aee00da3f73230c751e2462e3fddfd3eab95
Dwayne Edwards <kdedwards@gmail.com>
2020-12-27 23:31:09 -0500

    Day 6 linting fixes

--- Day 06/customsprocessorp1.py
+++ Day 06/customsprocessorp1.py
@@ -1,35 +1,35 @@
 with open("customsanswers.dat") as customsData:
     customsAnswersRaw = customsData.readlines()
 
-answerGroups=[]
-thisAnswerGroup=[]
+answerGroups = []
+thisAnswerGroup = []
 
 for line in customsAnswersRaw:
-    if(line=='\n'):
+    if(line == '\n'):
         answerGroups.append(thisAnswerGroup)
-        #print(thisPassport)
-        thisAnswerGroup=[]
+        # print(thisPassport)
+        thisAnswerGroup = []
     else:
-        thisAnswerGroup.extend(line.replace('\n','').split(' '))
+        thisAnswerGroup.extend(line.replace('\n', '').split(' '))
 answerGroups.append(thisAnswerGroup)
 
-groupSummaries=[]
+groupSummaries = []
 
 for answerGroup in answerGroups:
-    groupSummary=[]
+    groupSummary = []
     for answer in answerGroup:
-        answers=list(answer)
+        answers = list(answer)
         for answer in answers:
             if(answer not in groupSummary):
                 groupSummary.append(answer)
     groupSummary.sort()
     groupSummaries.append(groupSummary)
-    #print('List: {} Len: {}'.format(str(groupSummary), len(groupSummary)))
+    # print('List: {} Len: {}'.format(str(groupSummary), len(groupSummary)))
 
-total=0
+total = 0
 for groupSummary in groupSummaries:
-    print('List: {} Len: {}'.format(str(groupSummary), len(groupSummary)))
-    total+=len(groupSummary)
-    #print(groupSummary.count())
+    # print('List: {} Len: {}'.format(str(groupSummary), len(groupSummary)))
+    total += len(groupSummary)
+    # print(groupSummary.count())
 
 print('Total answers: {}'.format(total))
--- Day 06/customsprocessorp2.py
+++ Day 06/customsprocessorp2.py
@@ -1,36 +1,36 @@
 with open("customsanswers.dat") as customsData:
     customsAnswersRaw = customsData.readlines()
 
-answerGroups=[]
-thisAnswerGroup=[]
+answerGroups = []
+thisAnswerGroup = []
 
 for line in customsAnswersRaw:
-    if(line=='\n'):
+    if(line == '\n'):
         answerGroups.append(thisAnswerGroup)
-        thisAnswerGroup=[]
+        thisAnswerGroup = []
     else:
-        thisAnswerGroup.extend(line.replace('\n','').split(' '))
+        thisAnswerGroup.extend(line.replace('\n', '').split(' '))
 answerGroups.append(thisAnswerGroup)
 
-groupSummaries=[]
+groupSummaries = []
 
 for answerGroup in answerGroups:
-    groupSummary={}
-    groupSummary['answerCount']=0
+    groupSummary = {}
+    groupSummary['answerCount'] = 0
     for answer in answerGroup:
-        groupSummary['answerCount']+=1
-        answers=list(answer)
+        groupSummary['answerCount'] += 1
+        answers = list(answer)
         for answer in answers:
             if(answer in groupSummary):
-                groupSummary[(answer)]+=1
+                groupSummary[(answer)] += 1
             else:
-                groupSummary[(answer)]=1
+                groupSummary[(answer)] = 1
     groupSummaries.append(groupSummary)
 
-allYesTotal=0
+allYesTotal = 0
 for groupSummary in groupSummaries:
     for key in groupSummary:
         if(groupSummary[key] == int(groupSummary['answerCount']) and key != 'answerCount'):
-            allYesTotal+=1
+            allYesTotal += 1
 
 print('Total answers that are all Yes: {}'.format(allYesTotal))
