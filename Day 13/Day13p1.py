b99dde804a6ee8480fc7f5d7ae0f7af453aea8a9
Dwayne Edwards <kdedwards@gmail.com>
2020-12-28 23:36:47 -0500

    Swapped p1 and p2 files

--- Day 13/Day13p1.py
+++ Day 13/Day13p1.py
@@ -1,4 +1,3 @@
-# this is a WIP, does not work currently
 
 
 def readFileToList(fileName):
@@ -7,47 +6,15 @@ def readFileToList(fileName):
     return retData
 
 
-def normalizeBusses(busses):
-    i = 0
-    normalBusses = []
+busSchedule = readFileToList(r'insput.dat')
+startTime = int(busSchedule[0])
+busses = list(filter(('x').__ne__, busSchedule[1].split(',')))
+
+futureTime = startTime
+foundBus = False
+while not foundBus:
     for bus in busses:
-        if bus != 'x':
-            normalBusses.append([i, int(bus)])
-        i += 1
-    return normalBusses
-
-
-busSchedule = readFileToList(r'input-sample.dat')
-busSchedule = readFileToList(r'input.dat')
-# startTime = int(busSchedule[0])
-startTime = 0
-busses = busSchedule[1].split(',')
-
-normBusses = normalizeBusses(busses)
-
-allArrivalTimes = []
-for normBus in normBusses:
-    start = int(startTime / normBus[1])
-    arrivalTimes = set()
-    for i in range(200000000):
-        arrivalTimes.add(start + i * normBus[1] - normBus[0])
-    allArrivalTimes.append(arrivalTimes)
-# print(allArrivalTimes[0])
-commonArrivals = allArrivalTimes[0] & allArrivalTimes[1]
-for i in range(2, len(allArrivalTimes)):
-    commonArrivals = commonArrivals & allArrivalTimes[i]
-
-lstCommonArrivals = list(commonArrivals)
-lstCommonArrivals.sort()
-
-print(lstCommonArrivals[0])
-
-
-# futureTime = startTime
-# foundBus = False
-# while not foundBus:
-#    for bus in busses:
-#        if futureTime % int(bus) == 0:
-#            print('Take bus number {} at time {}. Magic number: {}'.format(bus, futureTime, int(bus) * (futureTime - startTime)))
-#            foundBus = True
-#    futureTime += 1
+        if futureTime % int(bus) == 0:
+            print('Take bus number {} at time {}. Magic number: {}'.format(bus, futureTime, int(bus) * (futureTime - startTime)))
+            foundBus = True
+    futureTime += 1
--- Day 13/Day13p2.py
+++ Day 13/Day13p2.py
@@ -1,3 +1,4 @@
+# this is a WIP, does not work currently
 
 
 def readFileToList(fileName):
@@ -6,15 +7,47 @@ def readFileToList(fileName):
     return retData
 
 
-busSchedule = readFileToList(r'C:\Users\dedwards\projects\2020-Advent\Day 13\input-sample.dat')
-startTime = int(busSchedule[0])
-busses = list(filter(('x').__ne__, busSchedule[1].split(',')))
-
-futureTime = startTime
-foundBus = False
-while not foundBus:
+def normalizeBusses(busses):
+    i = 0
+    normalBusses = []
     for bus in busses:
-        if futureTime % int(bus) == 0:
-            print('Take bus number {} at time {}. Magic number: {}'.format(bus, futureTime, int(bus) * (futureTime - startTime)))
-            foundBus = True
-    futureTime += 1
+        if bus != 'x':
+            normalBusses.append([i, int(bus)])
+        i += 1
+    return normalBusses
+
+
+busSchedule = readFileToList(r'input-sample.dat')
+busSchedule = readFileToList(r'input.dat')
+# startTime = int(busSchedule[0])
+startTime = 0
+busses = busSchedule[1].split(',')
+
+normBusses = normalizeBusses(busses)
+
+allArrivalTimes = []
+for normBus in normBusses:
+    start = int(startTime / normBus[1])
+    arrivalTimes = set()
+    for i in range(200000000):
+        arrivalTimes.add(start + i * normBus[1] - normBus[0])
+    allArrivalTimes.append(arrivalTimes)
+# print(allArrivalTimes[0])
+commonArrivals = allArrivalTimes[0] & allArrivalTimes[1]
+for i in range(2, len(allArrivalTimes)):
+    commonArrivals = commonArrivals & allArrivalTimes[i]
+
+lstCommonArrivals = list(commonArrivals)
+lstCommonArrivals.sort()
+
+print(lstCommonArrivals[0])
+
+
+# futureTime = startTime
+# foundBus = False
+# while not foundBus:
+#    for bus in busses:
+#        if futureTime % int(bus) == 0:
+#            print('Take bus number {} at time {}. Magic number: {}'.format(bus, futureTime, int(bus) * (futureTime - startTime)))
+#            foundBus = True
+#    futureTime += 1
