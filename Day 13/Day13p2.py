5609c948d01e646ac43e740de257c302b7a87e63
Dwayne Edwards <kdedwards@gmail.com>
2020-12-29 00:40:48 -0500

    Cleaning up

--- Day 13/Day13p2.py
+++ Day 13/Day13p2.py
@@ -1,4 +1,4 @@
-# this is a WIP, does not work currently
+# This is a WIP. The approach to the problem by brute forcing is probably not realistic or will produce a solution within a reasonable amount of time.
 
 
 def readFileToList(fileName):
@@ -17,37 +17,27 @@ def normalizeBusses(busses):
     return normalBusses
 
 
-busSchedule = readFileToList(r'input-sample.dat')
-busSchedule = readFileToList(r'input.dat')
-# startTime = int(busSchedule[0])
-startTime = 0
+busSchedule = readFileToList(r'C:\Users\dedwards\projects\2020-Advent\Day 13\input.dat')
 busses = busSchedule[1].split(',')
-
 normBusses = normalizeBusses(busses)
-
-allArrivalTimes = []
-for normBus in normBusses:
-    start = int(startTime / normBus[1])
-    arrivalTimes = set()
-    for i in range(200000000):
-        arrivalTimes.add(start + i * normBus[1] - normBus[0])
-    allArrivalTimes.append(arrivalTimes)
-# print(allArrivalTimes[0])
-commonArrivals = allArrivalTimes[0] & allArrivalTimes[1]
-for i in range(2, len(allArrivalTimes)):
-    commonArrivals = commonArrivals & allArrivalTimes[i]
-
-lstCommonArrivals = list(commonArrivals)
-lstCommonArrivals.sort()
-
-print(lstCommonArrivals[0])
-
-
-# futureTime = startTime
-# foundBus = False
-# while not foundBus:
-#    for bus in busses:
-#        if futureTime % int(bus) == 0:
-#            print('Take bus number {} at time {}. Magic number: {}'.format(bus, futureTime, int(bus) * (futureTime - startTime)))
-#            foundBus = True
-#    futureTime += 1
+# print(normBusses)
+arrivalStart = 4347826086956
+arrivalWindowSize = 1000000
+arrivalStartFound = False
+while not arrivalStartFound:
+    allArrivalTimes = []
+    print('Searching times: {} - {}'.format(arrivalStart, arrivalStart + arrivalWindowSize))
+    for normBus in normBusses:
+        arrivalTimes = set()
+        for i in range(1, arrivalWindowSize):
+            arrivalTimes.add(arrivalStart + i * normBus[1] - normBus[0] + normBusses[0][1])
+        allArrivalTimes.append(arrivalTimes)
+    commonArrivals = allArrivalTimes[0] & allArrivalTimes[1]
+    for i in range(2, len(allArrivalTimes)):
+        commonArrivals = commonArrivals & allArrivalTimes[i]
+    if len(commonArrivals) != 0:
+        lstCommonArrivals = list(commonArrivals)
+        lstCommonArrivals.sort()
+        print(lstCommonArrivals[0])
+        arrivalStartFound = True
+    arrivalStart = arrivalStart + arrivalWindowSize
