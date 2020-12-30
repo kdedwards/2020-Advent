c6bb42c2cfa9b52f64ea53829c3ad5bc2caaf2ca
Dwayne Edwards <kdedwards@gmail.com>
2020-12-30 13:01:56 -0500

    Removed some debugging code

--- Day 15/Day15p1.py
+++ Day 15/Day15p1.py
@@ -2,8 +2,6 @@ numbersSpoken = [0, 3, 6]
 iterations = 2020
 
 for i in range(len(numbersSpoken), iterations):
-    if i % 1000000 == 0:
-        print(iterations)
     if numbersSpoken.count(numbersSpoken[i - 1]) == 1:
         numbersSpoken.append(0)
     else:
