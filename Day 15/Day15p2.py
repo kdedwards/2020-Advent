f57d71621e2bfd615739541b75226af24d793533
Dwayne Edwards <kdedwards@gmail.com>
2020-12-30 15:37:48 -0500

    wip

--- Day 15/Day15p2.py
+++ Day 15/Day15p2.py
@@ -4,15 +4,23 @@ numbersSpoken = {
     3: 1,
     6: 2
 }
-iterations = 5
+iterations = 7
 lastNumber = list(numbersSpoken.keys())[-1]
-
+print('Starting number: {}'.format(lastNumber))
+print(numbersSpoken)
 for i in range(len(numbersSpoken), iterations):
-    print('Last number: {}.'.format(lastNumber))
-    if lastNumber not in numbersSpoken:
+    print('Iteration: {} numbersSpoken: {}'.format(i, numbersSpoken))
+    if (lastNumber not in numbersSpoken.keys()) != (numbersSpoken[lastNumber] == (i - 1)):
+        print('Number {} never spoken.'.format(lastNumber))
         lastNumber = 0
-        numbersSpoken[0] = i
-    elif numbersSpoken[lastNumber] != i - 1:
+    elif (lastNumber in numbersSpoken) and (numbersSpoken[lastNumber] == (i - 1)):
+        lastNumber = i - numbersSpoken[lastNumber]
+        print('1-Number {} was exact previous number'.format(lastNumber))
+    else:
+        print('2-Number {} was not previous number'.format(lastNumber))
+    numbersSpoken[lastNumber] = i
+    print('Last number: {}.'.format(lastNumber))
+print(numbersSpoken)
 
 # for i in range(len(numbersSpoken), iterations):
 #    if numbersSpoken.count(numbersSpoken[i - 1]) == 1:
@@ -22,7 +30,4 @@ for i in range(len(numbersSpoken), iterations):
 #        lastTimeSpoken = numbersSpoken.index(numbersSpoken[0], 1)
 #        numbersSpoken.reverse()
 #        numbersSpoken.append(lastTimeSpoken)
-
-print(numbersSpoken)
-
 #print('Last number spoken: {}.'.format(numbersSpoken[-1]))
