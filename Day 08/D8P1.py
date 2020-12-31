9236ffa31d2f67ce7d10539e067a46b00d45b771
Dwayne Edwards <kdedwards@gmail.com>
2020-12-31 02:26:40 -0500

    Fixing linting errors

--- Day 08/D8P1.py
+++ Day 08/D8P1.py
@@ -4,25 +4,25 @@ def readFileToList(fileName):
     return retData
 
 
-instructions=readFileToList(r'instructions-p1.dat')
-executions=[0] * len(instructions)
-accumulator=0
-instructionPointer=0
+instructions = readFileToList(r'instructions-p1.dat')
+executions = [0] * len(instructions)
+accumulator = 0
+instructionPointer = 0
 print(instructions)
 while True:
-    if(executions[instructionPointer]>=1):
-        print('Program will loop executing this instruction {} instructionPointer at {}. Accumulator value: {}'.format(instructions[instructionPointer],instructionPointer,accumulator))
+    if(executions[instructionPointer] >= 1):
+        print('Program will loop executing this instruction {} instructionPointer at {}. Accumulator value: {}'.format(instructions[instructionPointer], instructionPointer, accumulator))
         break
-    instruction=instructions[instructionPointer].split(' ')[0]
-    arg=instructions[instructionPointer].split(' ')[1]
+    instruction = instructions[instructionPointer].split(' ')[0]
+    arg = instructions[instructionPointer].split(' ')[1]
     print('SP: {} Op: {} Arg: {}'.format(instructionPointer, instruction, arg))
-    if(instruction=='nop'):
-        executions[instructionPointer]+=1
-        instructionPointer+=1
-    elif(instruction=='acc'):
-        executions[instructionPointer]+=1
-        instructionPointer+=1
-        accumulator+=int(arg)
-    elif(instruction=='jmp'):
-        executions[instructionPointer]+=1
-        instructionPointer+=int(arg)
+    if(instruction == 'nop'):
+        executions[instructionPointer] += 1
+        instructionPointer += 1
+    elif(instruction == 'acc'):
+        executions[instructionPointer] += 1
+        instructionPointer += 1
+        accumulator += int(arg)
+    elif(instruction == 'jmp'):
+        executions[instructionPointer] += 1
+        instructionPointer += int(arg)
