9880e4fdfb176bdf57a653d661139d829f69256a
Dwayne Edwards <kdedwards@gmail.com>
2020-12-31 02:33:56 -0500

    Fixing linting errors

--- Day 09/D9P1.py
+++ Day 09/D9P1.py
@@ -3,26 +3,27 @@ def readFileToList(fileName):
         retData = Data.read().split('\n')
     return retData
 
-def findOutlier(data,window):
-    startIndex=0
-    while startIndex+window<len(data):
-        testList=data[startIndex:startIndex+window]
-        testSum=data[startIndex+window]
-        #print('Looking in list: {} for {}'.format(testList,testSum))
-        foundAddends=False
-        for i in range(0, window-1):
-            for j in range(i+1, window):
-                #print('Does {} == {}?'.format(int(testList[i])+int(testList[j]),testSum))
-                if(int(testList[i])+int(testList[j])==int(testSum)):
-                    foundAddends=True
+
+def findOutlier(data, window):
+    startIndex = 0
+    while startIndex + window < len(data):
+        testList = data[startIndex:startIndex + window]
+        testSum = data[startIndex + window]
+        # print('Looking in list: {} for {}'.format(testList,testSum))
+        foundAddends = False
+        for i in range(0, window - 1):
+            for j in range(i + 1, window):
+                # print('Does {} == {}?'.format(int(testList[i])+int(testList[j]),testSum))
+                if(int(testList[i]) + int(testList[j]) == int(testSum)):
+                    foundAddends = True
                     break
             if foundAddends:
                 break
-        if foundAddends==False:
-            #print('Unable to find addends for {}'.format(testSum))
+        if foundAddends is False:
+            # print('Unable to find addends for {}'.format(testSum))
             return testSum
-        startIndex+=1
+        startIndex += 1
 
 
-XMASdata=readFileToList(r'input.dat')
-print('Unable to find addends for: {}'.format(findOutlier(XMASdata,25)))
+XMASdata = readFileToList(r'input.dat')
+print('Unable to find addends for: {}'.format(findOutlier(XMASdata, 25)))
