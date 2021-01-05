ab7c9e47f08a6073b05f16e3f8d52259d951b00c
Dwayne Edwards <kdedwards@gmail.com>
2021-01-04 22:35:48 -0500

    Refactored a bit

--- Day 01/Report_Repair.py
+++ Day 01/Report_Repair.py
@@ -1,13 +1,16 @@
 with open("Report_Repair.dat") as expenseData:
-    expenses = expenseData.read().split('\n')
+    expenseLines = expenseData.read().split('\n')
+
+# Comprehension to convert all of the strings read in from the file to ints
+expenses = [int(expenseLine) for expenseLine in expenseLines]
 
 for expense in expenses:
     for secExpense in expenses[expenses.index(expense) + 1::]:
-        if(int(expense) + int(secExpense) == 2020):
-            print('Found it! Expense1: {} Expense2: {} Product {}'.format(expense, secExpense, int(expense) * int(secExpense)))
+        if(expense + secExpense == 2020):
+            print('Found it! Expense1: {} Expense2: {} Product {}'.format(expense, secExpense, expense * secExpense))
 
 for expense in expenses:
     for secExpense in expenses[expenses.index(expense) + 1::]:
         for thirExpense in expenses[expenses.index(secExpense) + 1::]:
-            if(int(expense) + int(secExpense) + int(thirExpense) == 2020):
-                print('Found it! Expense1: {} Expense2: {} Expense3: {} Product {}'.format(expense, secExpense, thirExpense, int(expense) * int(secExpense) * int(thirExpense)))
+            if(expense + secExpense + thirExpense == 2020):
+                print('Found it! Expense1: {} Expense2: {} Expense3: {} Product {}'.format(expense, secExpense, thirExpense, expense * secExpense * thirExpense))
